de0431bc787a8b2ae281e6c3b89f799c34f84a51
thescottish1 <haggison68@hotmail.com>
2016-02-02 09:25:58 -0500

    2nd Initial Upload Entity Model Flyer Application

---
+++ FlyerV2/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FlyerV2.Startup))]
+namespace FlyerV2
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
